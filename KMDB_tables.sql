9bb5bf93dd4c7675fe8c950983e4dd04769b53fc
sdesai824 <sahil.desai@kellogg.northwestern.edu>
2023-01-22 00:53:05 +0000

    set-up drop tables for KIMDB domain model

---
+++ KMDB_tables.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id INTEGER,
+  actor_id INTEGER,
+  first_name TEXT,
+  last_name TEXT
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  studio_id INTEGER,
+  actor_id INTEGER,
+  title TEXT,
+  year_released TEXT,
+  mpaa_rating TEXT
+);
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  studio_name TEXT
+);
